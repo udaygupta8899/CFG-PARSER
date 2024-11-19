934cad458b44f5551d67bca43092cb9d074f0a19
udaygupta8899 <149859386+udaygupta8899@users.noreply.github.com>
2024-11-19 22:48:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -225,7 +225,7 @@ st.markdown("""
     <div style="position: fixed; bottom: 10px; right: 10px; padding: 10px; border: 2px solid #4CAF50; border-radius: 5px; background-color: #333333; color: white;">
         <h4 style="margin: 0; font-weight: bold;">Team Members:</h4>
         <p style="margin: 5px 0;">Uday Kumar Gupta (2023UAI1800)</p>
-        <p style="margin: 0;">Gaurav Sharma (2023UAI18)</p>
+        <p style="margin: 0;">Gaurav Sharma (2023UAI1810)</p>
     </div>
 """, unsafe_allow_html=True)
 
